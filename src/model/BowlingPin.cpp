c1206a216f9fb73a6077bf7ff8781d2cc0fc2021
Klaas van Gend <klaas@opentux.nl>
2009-05-30 19:24:51 +0000

    replace cylinder modelling with box modelling. This works a lot better - cylinder support in ODE is apparently a little flakey :-(

--- src/model/BowlingPin.cpp
+++ src/model/BowlingPin.cpp
@@ -35,27 +35,18 @@ static BowlingPinObjectFactory theFactory;
 
 BowlingPin::BowlingPin ( ) 
 {
-	// TODO/FIXME: for now, the pin is defined lying - not standing!!! 
 	DEBUG5("BowlingPin::BowlingPin\n");
-	// set the bowling pin to be a cylinder
+	// set the bowling pin to be a *box* - not a cylinder
 	const dReal myRadius = 0.06; // m
 	const dReal myLength = 0.38; // m
 	const dReal myMass   = 1.5;  // kg
-	setTheGeomID( dCreateCylinder (getSpaceID(), myRadius, myLength) );
-	setMassCylinder(myMass, AlongZAxis, myRadius, myLength);
+	setTheGeomID( dCreateBox (getSpaceID(), 2*myRadius, myLength, 1.0) );
+	setMassBox(myMass, 2*myRadius, myLength);
 	
-	// the dCreateCylinder creates a cylinder along the Z axis.
-	dMatrix3 R;
-	dRFromAxisAndAngle(R, 1.0, 0.0, 0.0, -PI/2);
-	dGeomSetRotation(theGeomID, R);
-	dMassRotate(&theMass, R);
-	
-	
-	setTheBounciness(1.0);
+	setTheBounciness(1.1);
 	
 	setTheWidth(2.0*myRadius);
 	setTheHeight(myLength);
-//	setAngle(PI/2.0);
 }
 
 BowlingPin::~BowlingPin ( )
