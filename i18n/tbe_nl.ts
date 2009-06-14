d6a3b7ba2c3637d9986ebe5c9f5f2533b28835b9
Klaas van Gend <klaas@opentux.nl>
2009-06-14 23:02:28 +0000

    move everything to trunk, part 2

---
+++ i18n/tbe_nl.ts
@@ -0,0 +1,301 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL">
+<context>
+    <name>Level</name>
+    <message>
+        <location filename="../src/model/Level.cpp" line="73"/>
+        <source>Cannot read file &apos;%1&apos;</source>
+        <translation>Kan bestand &apos;%1&apos; niet lezen</translation>
+    </message>
+    <message>
+        <location filename="../src/model/Level.cpp" line="90"/>
+        <source>Cannot parse file - not valid XML?</source>
+        <translatorcomment>Ik ben niet heel blij met parse&gt;lezen...</translatorcomment>
+        <translation type="unfinished">Kan bestand niet lezen - geen geldige XML?</translation>
+    </message>
+    <message>
+        <location filename="../src/model/Level.cpp" line="100"/>
+        <location filename="../src/model/Level.cpp" line="118"/>
+        <location filename="../src/model/Level.cpp" line="130"/>
+        <location filename="../src/model/Level.cpp" line="134"/>
+        <location filename="../src/model/Level.cpp" line="178"/>
+        <source>Parsing &apos;%1&apos; section failed</source>
+        <translation type="unfinished">Lezen van paragraaf &apos;%1&apos; mislukt</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/view/MainWindow.cpp" line="54"/>
+        <source>&amp;Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.cpp" line="55"/>
+        <source>Ctrl+Z</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.cpp" line="58"/>
+        <source>&amp;Redo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.cpp" line="60"/>
+        <source>Ctrl+Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.cpp" line="60"/>
+        <source>Shift+Ctrl+Z</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.cpp" line="75"/>
+        <source>&lt;b&gt;The Butterfly Effect&lt;/b&gt;&lt;br&gt;&lt;br&gt;An open source game that uses realistic physics simulations to combine lots of simple mechanical elements to achieve a simple goal in the most complex way possible.&lt;br&gt;&lt;br&gt;(C) 2009 Peter van Ginneken and Klaas van Gend&lt;br&gt;Licensed under GPL version 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.cpp" line="88"/>
+        <source>Open Level</source>
+        <translation>Open Level</translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.cpp" line="88"/>
+        <source>TBE Levels (*.xml *.tbe)</source>
+        <translation>TBE Levels (*.xml *.tbe)</translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="20"/>
+        <source>The Butterfly Effect</source>
+        <translatorcomment>please do not translate</translatorcomment>
+        <translation>The Butterfly Effect</translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="36"/>
+        <source>The Tool Box</source>
+        <translation type="unfinished">Gereedschapskist</translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="73"/>
+        <source>Game Controls</source>
+        <translation type="unfinished">Spelbesturing</translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="82"/>
+        <location filename="../src/view/MainWindow.ui" line="223"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="92"/>
+        <source>Reset</source>
+        <translation type="unfinished">Terugstellen</translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="112"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="124"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="129"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="135"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="144"/>
+        <source>&amp;Simulation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="162"/>
+        <source>Save...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="167"/>
+        <source>About...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="175"/>
+        <source>Restart Level...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="183"/>
+        <source>&amp;Open savegame...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="191"/>
+        <source>Zoom to all...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="199"/>
+        <source>Zoom In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="202"/>
+        <source>Ctrl++</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="210"/>
+        <source>Zoom Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="213"/>
+        <source>Ctrl+-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="218"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="228"/>
+        <source>S&amp;top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="233"/>
+        <source>&amp;Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/MainWindow.ui" line="238"/>
+        <source>Open level</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PieMenu</name>
+    <message>
+        <location filename="../src/view/PieMenu.cpp" line="56"/>
+        <source>Move %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/PieMenu.cpp" line="57"/>
+        <source>In this mode, you can move %1 around</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/PieMenu.cpp" line="59"/>
+        <source>Rotate</source>
+        <translation>Roteren</translation>
+    </message>
+    <message>
+        <location filename="../src/view/PieMenu.cpp" line="60"/>
+        <source>In this mode, you can rotate %1</source>
+        <translation>In deze modus kun je %1 roteren</translation>
+    </message>
+    <message>
+        <location filename="../src/view/PieMenu.cpp" line="62"/>
+        <source>Resize horizontally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/PieMenu.cpp" line="63"/>
+        <source>In this mode, you can make %1 wider</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/PieMenu.cpp" line="65"/>
+        <source>Resize vertically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/view/PieMenu.cpp" line="66"/>
+        <source>In this mode, you can make %1 taller</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/model/BowlingBall.h" line="56"/>
+        <source>Bowling Ball</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/BowlingBall.h" line="62"/>
+        <source>Your average bowling ball - heavy, round and willing to roll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/BowlingPin.h" line="56"/>
+        <source>Bowling Pin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/BowlingPin.h" line="62"/>
+        <source>Bowling pins are meant to be run over - and most people prefer to do that using a Bowling Ball.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/Floor.h" line="55"/>
+        <source>Floor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/Floor.h" line="61"/>
+        <source>This is a floor piece: flat and horizontal.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/Ramp.h" line="55"/>
+        <source>Ramp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/Ramp.h" line="61"/>
+        <source>This is a ramp.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/Wall.h" line="55"/>
+        <source>Wall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/model/Wall.h" line="61"/>
+        <source>This is a wall piece: vertical of course.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StartStopReset</name>
+    <message>
+        <location filename="../src/view/StartStopReset.cpp" line="168"/>
+        <source>START</source>
+        <translation>BEGIN</translation>
+    </message>
+    <message>
+        <location filename="../src/view/StartStopReset.cpp" line="172"/>
+        <source>CONTINUE</source>
+        <translation>GA DOOR</translation>
+    </message>
+    <message>
+        <location filename="../src/view/StartStopReset.cpp" line="175"/>
+        <source>STOP</source>
+        <translation>STOP</translation>
+    </message>
+</context>
+</TS>
