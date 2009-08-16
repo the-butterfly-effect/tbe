feb07a19a8d92d17042add325b3f368152fcfa42
Klaas van Gend <klaas@opentux.nl>
2009-08-16 15:01:55 +0000

    improve bowling pin shape a bit.

--- src/model/BowlingPin.cpp
+++ src/model/BowlingPin.cpp
@@ -43,58 +43,20 @@ BowlingPin::BowlingPin ( )
 	const qreal myLength = 0.38; // m
 	const qreal myMass   = 1.5;  // kg
 	
-	b2PolygonDef* boxDef = new b2PolygonDef();
-
-	// remember, SetAsBox takes half width and half height
-	// and will center around the center - exactly what we want :-)
-	boxDef->SetAsBox  	( myRadius, myLength/2.0);
-
-	boxDef->density = myMass/(2*myRadius*myLength);
-
+	b2PolygonDef* my5PointPinDef = new b2PolygonDef();
+	// TODO: Fix density
+	my5PointPinDef->density = myMass/(2*myRadius*myLength);
+	my5PointPinDef->vertexCount = 5;
+	my5PointPinDef->vertices[0].Set( 0   ,  0.17);
+	my5PointPinDef->vertices[1].Set(-0.06,  0);
+	my5PointPinDef->vertices[2].Set(-0.03, -0.17);
+	my5PointPinDef->vertices[3].Set( 0.03, -0.17);
+	my5PointPinDef->vertices[4].Set( 0.06,  0);
 	// delete any shapes on the body
 	// and create a new shape from the above polygon def
-	theShapeList.push_back(boxDef);
+	theShapeList.push_back(my5PointPinDef);
 
-//	// top (10)
-//	b2PolygonDef* partDef = new b2PolygonDef();
-//	// TODO: make density across all polygons
-//	partDef->density = myMass/(2*myRadius*myLength);
-//	partDef->vertexCount = 3;
-//	partDef->vertices[0].Set(-00.318, 03.493);
-//	partDef->vertices[1].Set(00.318, 03.493);
-//	partDef->vertices[2].Set(0, 03.81);
-//	theShapeList.push_back(partDef);
-//
-//	// right-top (9)
-//	partDef = new b2PolygonDef();
-//	partDef->vertexCount = 3;
-//	partDef->vertices[0].Set(2.29, 34.93);
-//	partDef->vertices[1].Set(2.29, 25.4);
-//	partDef->vertices[2].Set(3.18, 25.4);
-////	theShapeList.push_back(partDef);
-//
-//	// mid-top (8)
-//	partDef = new b2PolygonDef();
-//	partDef->vertexCount = 4;
-//	partDef->vertices[0].Set( 2.29,	33);
-//	partDef->vertices[1].Set( 2.29,	34.93);
-//	partDef->vertices[2].Set(-2.29, 34.93);
-//	partDef->vertices[3].Set(-2.29, 33);
-////	theShapeList.push_back(partDef);
-//
-//
-//	// mid-mid (4)
-//	partDef = new b2PolygonDef();
-//	// TODO: make density across all polygons
-//	partDef->density = myMass/(2*myRadius*myLength);
-//	partDef->vertexCount = 4;
-//	partDef->vertices[0].Set( 0.229, 1.143);
-//	partDef->vertices[1].Set( 0.229, 2.54);
-//	partDef->vertices[2].Set(-0.229, 2.54);
-//	partDef->vertices[3].Set(-0.229, 1.143);
-//	theShapeList.push_back(partDef);
-
-	setTheBounciness(0.85);
+	setTheBounciness(0.4);
 	
 	setTheWidth(2.0*myRadius);
 	setTheHeight(myLength);
