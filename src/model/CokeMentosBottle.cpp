9bea4e4c4be2a2ba15ab26be3f88bb7de63626b8
Klaas van Gend <klaas@opentux.nl>
2009-09-02 20:43:10 +0000

    This version of CokeMentosBottle already blows :-) There's a lot wrong with it, but at least it works a bit.

--- src/model/CokeMentosBottle.cpp
+++ src/model/CokeMentosBottle.cpp
@@ -20,6 +20,10 @@
 #include "tbe_global.h"
 #include "Box2D.h"
 
+/// Note: CokeSplatter does not have an ObjectFactory
+/// as it cannot be instantiated by a user (neither in-game
+/// nor in the level editor)
+
 
 //// this class' ObjectFactory
 class CokeMentosObjectFactory : public ObjectFactory
@@ -31,6 +35,7 @@ public:
 	{	return new CokeMentosBottle(); }
 };
 static CokeMentosObjectFactory theCMBottleObjectFactory;
+
 
 
 CokeMentosBottle::CokeMentosBottle()
@@ -49,7 +54,7 @@ CokeMentosBottle::CokeMentosBottle()
 	my5PointPinDef->vertices[3].Set( 0.075, -0.251);
 	my5PointPinDef->vertices[4].Set( 0.083,  0.12);
 	// approximation of the initial mass - we'll fix it later on...
-	my5PointPinDef->density = 1.6 / (0.166*0.501);
+	my5PointPinDef->density = 2.1 / (0.166*0.501);
 	
 	// delete any shapes on the body
 	// and create a new shape from the above polygon def
@@ -61,6 +66,15 @@ CokeMentosBottle::CokeMentosBottle()
 void CokeMentosBottle::callbackStep (qreal aTimeStep, qreal aTotalTime)
 {
 	DEBUG5("coke receives callback\n");
+
+	// this is just a simple delay/rate limiter
+	static int myBlowcount = 0;
+	myBlowcount++;
+	if (myBlowcount > 50 && ((myBlowcount%4)==0))
+	{
+		static int mySplatterCount = 0;
+		newSplatter(mySplatterCount++);
+	}
 }
 
 void CokeMentosBottle::reset(void)
@@ -75,7 +89,7 @@ void CokeMentosBottle::setBottleStatus(BottleStatus aNewStat)
 	switch(aNewStat)
 	{
 	case UNTRIGGERED:
-		theCokeAmount = 1.5;
+		theCokeAmount = 2.0;
 		break;
 	case TRIGGERED:
 		break;
@@ -96,4 +110,123 @@ void CokeMentosBottle::updateMass(void)
 
 	// test
 	myMass.mass = 0;
+}
+
+void CokeMentosBottle::newSplatter(unsigned int aSequenceNr)
+{
+	// the first splatter is to reach 1.5m height, improving to 2.5m with the 20th
+	// then deteriorating quickly - until none left
+	//
+	// for simplicity, we assume that a horizontal bottle has the same
+	// exit velocity and such...
+	//
+	// the opening exit velocity can be calculated using energy:  m*g*h = 0.5*m*v*v
+	// i.e.  v = sqrt(2*g*h)    <- note that the mass of the splatter is not in there
+
+	// so we need a 2nd order equation that starts at (0, 1.5), reaches (20,2.5)
+	// and then decays until (40, 1.5) again.
+	// then we use a linear equation for the remainder
+	//
+	//   h=height, n=splatternumber
+	// i.e. h = -1/400*n*n + 1/10*n + 1.5
+	// and from 40 onwards:
+	//      h = 1.5 - 3*(n-40)/220
+	//
+	// TODO/FIXME: there is a discontinuity in h' - that can be fixed by calculating
+	// the point where the extension of h' goes through (150,0) -> for later :-)
+
+	CokeSplatter* mySplatter = new CokeSplatter();
+
+	// start position for the splatter is above the opening: (0, +0.53*object height)
+	Position myStartPos = getTempCenter();
+
+	// the bottle position contains the angle, we need that to compensate for
+	// rotated bottles
+	qreal myAngle = myStartPos.angle;
+	myStartPos = myStartPos + Position(getTheHeight()*-0.53*sin(myAngle), getTheHeight()*0.53*cos(myAngle), PI/2.0);
+
+	// departure velocity
+	qreal myH;
+	if (aSequenceNr<40)
+		myH = (-1/400)*aSequenceNr*aSequenceNr + 0.1*aSequenceNr + 1.5;
+	else
+		myH = 1.5 - 3*(aSequenceNr-40)/220;
+
+	// if the expected height is less than 0, we're no longer blowing
+	if (myH <0.0)
+		return;
+
+	// note: hardcoded constant for G here, because we're using constants from the
+	// normal world to calculate it - not related to the G in a alternative world.
+	// (otherwise, the coke bottle would blow as high on the moon as on earth)
+	qreal myV = sqrt(2.0 * 9.81 * myH);
+	const qreal mySplatterMass = 0.01;
+	mySplatter->setAll(theWorldPtr, myStartPos, myV, mySplatterMass);
+
+	theCokeAmount -= mySplatterMass;
+	updateMass();
+
+	// TODO: don't forget Newton's action = -reaction;
+	//theB2BodyPtr->ApplyImpulse();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+CokeSplatter::CokeSplatter()
+		: RectObject(), isResetForReal(false)
+{
+	DEBUG5("CokeSplatter::CokeSplatter\n");
+	setProperty(IMAGE_NAME_STRING, "CokeSplatter");
+	setProperty(DESCRIPTION_STRING, "");
+
+	b2CircleDef* myBubbleDef = new b2CircleDef();
+	myBubbleDef->radius = theRadius;
+	myBubbleDef->density= 0.01 / (PI * theRadius * theRadius);
+
+	theShapeList.push_back(myBubbleDef);
+	setTheBounciness(0.0);
+	setTheWidth(2*theRadius);
+	setTheHeight(2*theRadius);
+}
+
+CokeSplatter::~CokeSplatter()
+{
+}
+
+void CokeSplatter::reset()
+{
+	DEBUG5("reset() body pos for 'CokeSplatter' %p\n", this);
+	if(isResetForReal==false)
+		return;
+	assert(false);
+	deregister();
+
+	// we need this, but let's not - for now
+	// delete this;
+}
+
+void CokeSplatter::setAll(World* aWorldPtr,
+						  const Position& aStartPos,
+						  qreal aVelocity,
+						  qreal aSplatterMass)
+{
+	DEBUG5("CokeSplatter::setAll(%p, (%f,%f,%f), %f, %f)\n",
+		   aWorldPtr, aStartPos.x, aStartPos.y, aStartPos.angle,
+		   aVelocity, aSplatterMass);
+
+	setOrigCenter(aStartPos);
+	aWorldPtr->addObject(this);
+	createPhysicsObject();
+
+	qreal myAngle = aStartPos.angle;
+	b2Vec2 myVelVec(aVelocity * cos(myAngle), aVelocity * sin(myAngle));
+	DEBUG5("velocity: %f,%f\n", myVelVec.x, myVelVec.y);
+	theB2BodyPtr->SetLinearVelocity(myVelVec);
+
+	isResetForReal = true;
+
+	// FIXME: variable mass is ignored for now...
 }
--- src/model/CokeMentosBottle.h
+++ src/model/CokeMentosBottle.h
@@ -22,6 +22,58 @@
 #include "RectObject.h"
 #include "World.h"
 
+/** this class implements the flying coke
+ *  it is modelled in small bubbles, 10 grams each
+ */
+class CokeSplatter : public RectObject
+{
+public:
+	CokeSplatter();
+	virtual ~CokeSplatter();
+
+	/// returns the Name of the object.
+	/// overridden from parent
+	virtual const QString getName ( ) const
+	{
+		// don't translate this one - not visible to user, ever...
+		return "CokeSplatter";
+	}
+
+
+	/// returns true if the object should not surive a World::reset()
+	/// overridden from BaseObject
+	virtual bool isTemp() const
+	{ return true; }
+
+	/** sets all parameters of the splatter, attaches to World
+	  * and creates the physical object and drawobject
+	  * @param aWorldPtr
+	  * @param aStartPos
+	  * @param aVelocity
+	  * @param aSplatterMass
+	  */
+	void setAll(World* aWorldPtr, const Position& aStartPos,
+				qreal aVelocity, qreal aSplatterMass);
+
+	/// reset() is called in two situations:
+	/// 1) by World on addObject() -> ignore this one
+	/// 2) by World on reset of World -> delete ourselves
+	/// see also isResetForReal .
+	/// overridden from RectObject
+	virtual void reset(void);
+
+protected:
+	/// overridden from RectObject to remove the functionality
+	virtual void adjustParameters(void)
+	{ ; };
+
+	const static qreal theRadius = 0.04;
+
+	/// true if we're outside initialisation.
+	/// in that case reset() will remove this object from World
+	bool isResetForReal;
+};
+
 /** this class implements the famous coke+mentos trick:
   * insert a mentos in a coke bottle and it will blow a
   * huge fountain - until the bottle is empty.
@@ -67,6 +119,11 @@ private:
 	/// implemented from SimStepCallbackInterface
 	virtual void callbackStep (qreal aTimeStep, qreal aTotalTime);
 
+	/** generate a new Coke splatter
+	 *  @param aSequenceNr the sequence number of the splatter
+	 */
+	void newSplatter(unsigned int aSequenceNr);
+	//(const Position& aStartPoint, qreal aVelocity, qreal aMass);
 
 private:
 	// Private things
@@ -77,7 +134,7 @@ private:
 
 	// Things from RectObject that need adjustments:
 
-	/// overridden to remove
+	/// overridden from RectObject to remove the functionality
 	virtual void adjustParameters(void)
 	{ ; };
 
@@ -86,7 +143,7 @@ private:
 	BottleStatus theBottleStatus;
 
 	/// the amount of liquid still in the bottle
-	/// 1.5l at start, reducing fast whilst blowing.
+	/// 2.0l at start, reducing fast whilst blowing.
 	/// unit: kg or l (who cares)
 	qreal	theCokeAmount;
 };
