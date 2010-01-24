74ca4c9cebabc3bec7ba6cface84238e9e946d8a
Klaas van Gend <klaas@opentux.nl>
2010-01-24 22:13:17 +0000

    update (C) in Help->About to include 2010.

--- src/view/MainWindow.cpp
+++ src/view/MainWindow.cpp
@@ -101,7 +101,7 @@ void MainWindow::on_actionAbout_activated()
 				"An open source game that uses realistic physics"
 				" simulations to combine lots of simple mechanical elements"
 				" to achieve a simple goal in the most complex way possible.<br><br>"
-				"(C) 2009 Peter van Ginneken and Klaas van Gend<br>"
+				"(C) 2009-2010 Peter van Ginneken and Klaas van Gend<br>"
 				"Licensed under GPL version 2"), this);
 }
 
