44f293fe22a39853e5f93d9b2ee3068889bc3cac
Klaas van Gend <klaas@opentux.nl>
2010-11-27 11:48:10 +0000

    fix ticket:237 for trunk - check whether i18n files actually exist before packaging

--- installer/make-win-installer.sh
+++ installer/make-win-installer.sh
@@ -4,4 +4,12 @@ unix2dos -n ../AUTHORS ../AUTHORS.txt
 unix2dos -n ../COPYING ../COPYING.txt
 unix2dos -n ../README  ../README.txt
 
+if [ ! -f ../i18n/tbe_nl.qm ];
+then 
+	echo "Oops... No i18n files built!"
+	exit;
+else
+	rm ../i18n/*.ts
+fi
+
 ../3rdParty/bin/installjammer --verbose --platform Windows --build-dir tmp --build $PWD/TheButterflyEffect.mpi
