148f5d8c0a00f0604b6c497315ee772a45814cc8
Klaas van Gend <klaas@opentux.nl>
2011-01-10 19:10:26 +0000

    Increase vertices to allow all of our objects again.

--- src/box2d/b2Settings.h
+++ src/box2d/b2Settings.h
@@ -47,7 +47,7 @@ typedef float float32;
 #define b2_maxManifoldPoints	2
 
 /// The maximum number of vertices on a convex polygon.
-#define b2_maxPolygonVertices	11
+#define b2_maxPolygonVertices	13
 
 /// This is used to fatten AABBs in the dynamic tree. This allows proxies
 /// to move by a small amount without triggering a tree adjustment.
