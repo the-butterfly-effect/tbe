f9c6c4618b8b8c094eae3a552c363119e2b92dd3
Klaas van Gend <klaas@opentux.nl>
2012-01-26 14:24:34 +0000

    Add extra safeguard for developers.

--- newgui/control/ViewObjectActionDectorator.cpp
+++ newgui/control/ViewObjectActionDectorator.cpp
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <cassert>
 #include "AbstractUndoCommand.h"
 #include "ImageCache.h"
 #include "MoveUndoCommand.h"
@@ -64,6 +65,7 @@ ViewObjectActionDecorator::mousePressEvent ( QGraphicsSceneMouseEvent* event )
 {
     // if there is no undo registered, let's delegate to someone else
     // (i.e. our parent)
+    assert(theAUCPtr!=NULL);
     if (theAUCPtr==NULL)
     {
         event->ignore();
