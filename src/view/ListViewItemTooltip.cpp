31ea555473183cfe9c69d45a4c431c81428baa0d
Klaas van Gend <klaas@opentux.nl>
2014-02-09 10:10:56 +0000

    Progress!

--- src/view/ListViewItemTooltip.cpp
+++ src/view/ListViewItemTooltip.cpp
@@ -1,9 +1,19 @@
 #include "ListViewItemTooltip.h"
+#include "ImageCache.h"
+#include "MainWindow.h"
+#include "ListViewItemTooltip.h"
+#include "ui_ListViewItemTooltip.h"
 
-
-ListViewItemTooltip::ListViewItemTooltip(QWidget *parent)
-    : QWidget(parent)
+ListViewItemTooltip::ListViewItemTooltip(ResizingGraphicsView* aParent) :
+    AnimatedDialog(aParent, AnimatedDialog::TOOLTIP),
+    ui(new Ui::ListViewItemTooltip)
 {
+    ui->setupUi(this);
+
+    // set the image, description and help
+
+    // make it appear(animated)
+    theYCoord = 50;
 
 }
 
@@ -11,4 +21,10 @@ ListViewItemTooltip::ListViewItemTooltip(QWidget *parent)
 ListViewItemTooltip::~ListViewItemTooltip()
 {
 
+}
+
+
+void ListViewItemTooltip::slotRemoveTooltip()
+{
+    deleteLater();
 }
--- src/view/ListViewItemTooltip.h
+++ src/view/ListViewItemTooltip.h
@@ -1,20 +1,25 @@
 #ifndef LISTVIEWITEMTOOLTIP_H
 #define LISTVIEWITEMTOOLTIP_H
 
-#include <QWidget>
+#include "animateddialog.h"
+#include "resizinggraphicsview.h"
+//#include <QWidget>
 
 namespace Ui {
 class ListViewItemTooltip;
 }
 
-class ListViewItemTooltip : public QWidget
+class ListViewItemTooltip : public AnimatedDialog
 {
     Q_OBJECT
     
 public:
-    explicit ListViewItemTooltip(QWidget *parent = 0);
+    explicit ListViewItemTooltip(ResizingGraphicsView* parent = 0);
     ~ListViewItemTooltip();
     
+public slots:
+    void slotRemoveTooltip();
+
 private:
     Ui::ListViewItemTooltip *ui;
 };
