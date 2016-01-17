4efd612129c663b4776006ee2843e029f89823fd
Klaas van Gend <klaas@vectorfabrics.com>
2016-01-17 10:33:22 +0100

    make i18n for Qt5 work

---
+++ test/level-i18n/i18n/tbe_de.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/tbe_global.h" line="22"/>
+        <source>The Butterfly Effect</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Test</name>
+    <message>
+        <location filename="../src/test.cpp" line="13"/>
+        <source>Hello, world: it&apos;s time to rock!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
