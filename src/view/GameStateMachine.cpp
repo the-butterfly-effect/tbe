32e0f5b64156b72cf285c70edb4f0ca33781eef5
Klaas van Gend <klaas@vectorfabrics.com>
2016-03-01 22:59:37 +0100

    set up the majority of the state transitions

--- src/view/GameStateMachine.cpp
+++ src/view/GameStateMachine.cpp
@@ -52,18 +52,18 @@ GameStateMachine::GameStateMachine(QObject *parent) :
     theProblemState            = new GameState(&theGameStateMachine, "ProblemState");
 
     theRunningState            = new GameState(&theGameStateMachine, "RunningState");
-    theRunningForwardSubState  = new GameState(&theRunningState,      "RunningForwardSubState");
-    theRunningNormalSubState   = new GameState(&theRunningState,      "RunningNormalSubState");
-    theRunningPausedSubState   = new GameState(&theRunningState,      "RunningPausedSubState");
-    theRunningRealFastSubState = new GameState(&theRunningState,      "RunningRealFastSubState");
-    theRunningSlowSubState     = new GameState(&theRunningState,      "RunningSlowSubState");
+    theRunningForwardSubState  = new GameState( theRunningState,      "RunningForwardSubState");
+    theRunningNormalSubState   = new GameState( theRunningState,      "RunningNormalSubState");
+    theRunningPausedSubState   = new GameState( theRunningState,      "RunningPausedSubState");
+    theRunningRealFastSubState = new GameState( theRunningState,      "RunningRealFastSubState");
+    theRunningSlowSubState     = new GameState( theRunningState,      "RunningSlowSubState");
     theRunningState->setInitialState(theRunningNormalSubState);
 
     theStoppedState            = new GameState(&theGameStateMachine, "StoppedState");
 
     theWonState                = new GameState(&theGameStateMachine, "WonState");
-    theWonPausedSubState       = new GameState(&theWonState,          "WonPausedSubState");
-    theWonRunningSubState      = new GameState(&theWonState,          "WonRunningSubState");
+    theWonPausedSubState       = new GameState( theWonState,          "WonPausedSubState");
+    theWonRunningSubState      = new GameState( theWonState,          "WonRunningSubState");
     theWonState->setInitialState(theWonRunningSubState);
 
     theGameStateMachine.setInitialState(theStoppedState);
@@ -71,37 +71,74 @@ GameStateMachine::GameStateMachine(QObject *parent) :
     // ****************************************************
     // setup all state transitions
 
-    // TODO: theFailedState->addTransition();
-    // etc
+    theFailedState->addTransition(this, SIGNAL(signal_Reset_triggered()), theStoppedState);
+    connect(theFailedState, SIGNAL(entered()), this, SIGNAL(signal_Stop_Gameplay()));
 
+    theProblemState->addTransition(this, SIGNAL(signal_Problems_solved()), theStoppedState);
+
+    theRunningState->addTransition(this, SIGNAL(signal_Fail_happened()), theFailedState);
+    theRunningState->addTransition(this, SIGNAL(signal_Won_happened()), theWonState);
+    theRunningState->addTransition(this, SIGNAL(signal_Reset_triggered()), theStoppedState);
+
+    //theRunningForwardSubState->addTransition(this, SIGNAL(signal_Forward_triggered()), theRunningForwardSubState);
+    theRunningForwardSubState->addTransition(this, SIGNAL(signal_Pause_triggered()), theRunningPausedSubState);
+    theRunningForwardSubState->addTransition(this, SIGNAL(signal_Play_triggered()), theRunningNormalSubState);
+    theRunningForwardSubState->addTransition(this, SIGNAL(signal_RealFast_triggered()), theRunningRealFastSubState);
+    theRunningForwardSubState->addTransition(this, SIGNAL(signal_Slow_triggered()), theRunningSlowSubState);
+
+    theRunningNormalSubState->addTransition(this, SIGNAL(signal_Forward_triggered()), theRunningForwardSubState);
+    theRunningNormalSubState->addTransition(this, SIGNAL(signal_Pause_triggered()), theRunningPausedSubState);
+    //theRunningNormalSubState->addTransition(this, SIGNAL(signal_Play_triggered()), theRunningNormalSubState);
+    theRunningNormalSubState->addTransition(this, SIGNAL(signal_RealFast_triggered()), theRunningRealFastSubState);
+    theRunningNormalSubState->addTransition(this, SIGNAL(signal_Slow_triggered()), theRunningSlowSubState);
+
+    theRunningPausedSubState->addTransition(this, SIGNAL(signal_Forward_triggered()), theRunningForwardSubState);
+    //theRunningPausedSubState->addTransition(this, SIGNAL(signal_Pause_triggered()), theRunningPausedSubState);
+    theRunningPausedSubState->addTransition(this, SIGNAL(signal_Play_triggered()), theRunningNormalSubState);
+    theRunningPausedSubState->addTransition(this, SIGNAL(signal_RealFast_triggered()), theRunningRealFastSubState);
+    theRunningPausedSubState->addTransition(this, SIGNAL(signal_Slow_triggered()), theRunningSlowSubState);
+
+    theRunningRealFastSubState->addTransition(this, SIGNAL(signal_Forward_triggered()), theRunningForwardSubState);
+    theRunningRealFastSubState->addTransition(this, SIGNAL(signal_Pause_triggered()), theRunningPausedSubState);
+    theRunningRealFastSubState->addTransition(this, SIGNAL(signal_Play_triggered()), theRunningNormalSubState);
+    //theRunningRealFastSubState->addTransition(this, SIGNAL(signal_RealFast_triggered()), theRunningRealFastSubState);
+    theRunningRealFastSubState->addTransition(this, SIGNAL(signal_Slow_triggered()), theRunningSlowSubState);
+
+    theRunningSlowSubState->addTransition(this, SIGNAL(signal_Forward_triggered()), theRunningForwardSubState);
+    theRunningSlowSubState->addTransition(this, SIGNAL(signal_Pause_triggered()), theRunningPausedSubState);
+    theRunningSlowSubState->addTransition(this, SIGNAL(signal_Play_triggered()), theRunningNormalSubState);
+    theRunningSlowSubState->addTransition(this, SIGNAL(signal_RealFast_triggered()), theRunningRealFastSubState);
+    //theRunningSlowSubState->addTransition(this, SIGNAL(signal_Slow_triggered()), theRunningSlowSubState);
+
+    theStoppedState->addTransition(this, SIGNAL(signal_Problems_arised()), theProblemState);
+    // TODO: figure out if immediately jump to substate or to master & retransmit
+    theStoppedState->addTransition(this, SIGNAL(signal_Forward_triggered()), theRunningForwardSubState);
+    theStoppedState->addTransition(this, SIGNAL(signal_Pause_triggered()), theRunningPausedSubState);
+    theStoppedState->addTransition(this, SIGNAL(signal_Play_triggered()), theRunningNormalSubState);
+    theStoppedState->addTransition(this, SIGNAL(signal_RealFast_triggered()), theRunningRealFastSubState);
+    theStoppedState->addTransition(this, SIGNAL(signal_Slow_triggered()), theRunningSlowSubState);
+    connect(theStoppedState, SIGNAL(entered()), this, SIGNAL(signal_Stop_Gameplay()));
+
+    theWonState->addTransition(this, SIGNAL(signal_Reset_triggered()), theStoppedState);
+    theWonRunningSubState->addTransition(this, SIGNAL(todo), theWonPausedSubState);
+    connect(theWonState, SIGNAL(entered()), this, SLOT(slot_SetWonRunningTimeout()));
+    theWonPausedSubState->addTransition(this, SIGNAL(signal_Reset_triggered()), theStoppedState);
 }
 
 
-void GameStateMachine::slot_Forward_triggered()
+void GameStateMachine::slot_NumberOfCrossesChanged(int aNewNumber)
 {
-
+    if (aNewNumber > 0)
+        emit signal_Problems_arised();
+    else
+        emit signal_Problems_solved();
 }
 
-
-void GameStateMachine::slot_Pause_triggered()
+void GameStateMachine::slot_SetWonRunningTimeout()
 {
-
-}
-
-
-void GameStateMachine::slot_Play_triggered()
-{
-
-}
-
-
-void GameStateMachine::slot_RealFast_triggered()
-{
-
-}
-
-
-void GameStateMachine::slot_Slow_triggered()
-{
-
+    theWonRunningTimer.setSingleShot(true);
+    /// This number represents how many miliseconds the simulation keeps
+    /// running after the level has been won.
+    /// TODO/FIXME: magic number here!!!
+    theWonRunningTimer.start(3000);
 }
