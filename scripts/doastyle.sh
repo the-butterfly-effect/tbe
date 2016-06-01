9739fbeb77527160c240f10462314edbb1294610
Aschwin Marsman <a.marsman@aynik.com>
2016-06-01 21:00:42 +0200

    Perform astyle with the correct settings on all files with the extensions .cpp, .h and .h.in Tested on OS X, should also work on Linux.

---
+++ scripts/doastyle.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+pathToRoot=`dirname $0`/..
+for extension in cpp h h.in
+do
+    astyle --style=kr --indent=spaces=4 --align-pointer=name --align-reference=name --convert-tabs --attach-namespaces --max-code-length=100 --max-instatement-indent=120 --pad-header --pad-oper `find ${pathToRoot} -type f -name "*.${extension}"|grep -v ^${pathToRoot}/src/Box2D|grep -v ^${pathToRoot}/build`
+done
