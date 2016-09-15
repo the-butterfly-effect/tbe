3b8609696fd2a1aa87fea7dcfa0ef20ddd8836eb
Klaas van Gend <klaas@opentux.nl>
2016-09-15 21:42:35 +0200

    Add prototype ResizeAwareQW to tree, but don't use yet.

---
+++ src/view/ResizeAwareQuickWidget.h
@@ -0,0 +1,62 @@
+/* The Butterfly Effect
+ * This file copyright (C) 2016 Klaas van Gend
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation
+ * applicable version is GPL version 2 only.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA.
+ */
+
+#ifndef RESIZEAWAREQUICKWIDGET_H
+#define RESIZEAWAREQUICKWIDGET_H
+
+#include "Position.h"
+#include <QQuickWidget>
+
+class ResizeAwareQuickWidget : public QQuickWidget
+{
+public:
+    ResizeAwareQuickWidget(QWidget *parent = Q_NULLPTR);
+
+    /// This property describes the width of a handle in pixels
+    Q_PROPERTY(int handleWidth MEMBER theHandleWidth NOTIFY handleWidthChanged)
+    /// This property describes the height of a handle in pixels
+    Q_PROPERTY(int handleHeight MEMBER theHandleHeight NOTIFY handleHeightChanged)
+
+    /// Property describes the number of pixels per meter. This number changes
+    /// if the user resizes the window. Horizontal version.
+    Q_PROPERTY(int hPixPerMeter READ hPixPerMeter NOTIFY hPixPerMeterChanged)
+    /// Property describes the number of pixels per meter. This number changes
+    /// if the user resizes the window. Vertical version.
+    Q_PROPERTY(int vPixPerMeter READ vPixPerMeter NOTIFY vPixPerMeterChanged)
+
+    /// Maps a coordinate inside the widget (in pixels) into the same
+    /// coordinate in the model world (meters).
+    Vector mapPixToMeters(qreal anX, qreal aY) const;
+    //Vector mapPixToMeters(QPointF aPixelPos) const;
+
+    /// Maps a coordinate in the model world (meters) into the same
+    /// coordinate inside the widget (in pixels).
+    QPointF mapMetersToPix(Vector aPos) const;
+    qreal mapMetersToPixX(qreal anX) const;
+    qreal mapMetersToPixY(qreal aY) const;
+
+signals:
+    void handleWidthChanged();
+    void handleHeightChanged();
+
+private:
+    int theHandleHeight;
+    int theHandleWidth;
+};
+
+#endif // RESIZEAWAREQUICKWIDGET_H
